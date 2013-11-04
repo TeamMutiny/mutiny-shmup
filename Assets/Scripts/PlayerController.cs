3b742b71cc446293e5a429ca287cdbab2cd4f42f
TeamMutiny <c111957@drdrb.com>
2013-11-04 10:56:18 +0200

    version 0.0.1 alpha

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private CharacterController playerController;
+	
+	public float speed;
+	
+	private Vector3 moveDirection;
+	
+	// Use this for initialization
+	void Start () {
+		moveDirection = new Vector3(0.0f,0.0f,0.0f);
+		
+		playerController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+	
+		moveDirection *= speed;
+		
+		playerController.Move(moveDirection * Time.deltaTime);
+		
+		
+	}
+}
